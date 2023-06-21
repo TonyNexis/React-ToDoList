da66c0fca875c67f7aa2118143ab39633d52b646
Anton Tymchyk <tony.zenekis@gmail.com>
2023-06-21 15:19:14 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+
+let num = 3;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <header className='App-header' >Hello world</header>
+      <p>Today we will study React in {num > 5 ? num : 'some'} days</p>
     </div>
   );
 }
