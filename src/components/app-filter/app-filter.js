298361600b8bdb4504bb82c78442b2ffc7442bb7
Anton <tony.zenekis@gmail.com>
2023-06-22 21:52:17 +0300

    app structure v0.5

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,3 +1,5 @@
+import './app-filter.css'
+
 const AppFilter = () => {
     return (
         <div className="btn-group">
@@ -7,15 +9,17 @@ const AppFilter = () => {
                     Все сотрудники
             </button>
             <button 
-                className="btn btn-outline-light "
+                className="btn btn-outline-light"
                 type='button'>
                     На повышение
             </button>
             <button 
-                className="btn btn-outline-light "
+                className="btn btn-outline-light"
                 type='button'>
                     З/П больше 1000$
             </button>
         </div>
     );
 }
+
+export default AppFilter;
