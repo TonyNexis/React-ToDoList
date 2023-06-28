8d057cde084f7444c1f05f8d04efa38760723d3a
Anton Tymchyk <tony.zenekis@gmail.com>
2023-06-28 15:03:48 +0300

    added event for deleting employers

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -7,30 +9,45 @@ import EmployeesAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Tony Nexis', salary: 600, increase: false, id: 1},
+                {name: 'Alex Monk', salary: 1200, increase: false, id: 2},
+                {name: 'John Karakey', salary: 800, increase: false, id: 3},
+                {name: 'Dan Brown', salary: 750, increase: true, id: 4},
+                {name: 'Yoko Shido', salary: 1500, increase: false, id: 5},
+                {name: 'Mala Karyka', salary: 350, increase: false, id: 6},
+            ]
+        }
+    }
 
-    const data = [
-        {name: 'Tony Nexis', salary: 600, increase: false, id: 1},
-        {name: 'Alex Monk', salary: 1200, increase: false, id: 2},
-        {name: 'John Karakey', salary: 800, increase: false, id: 3},
-        {name: 'Dan Brown', salary: 750, increase: true, id: 4},
-        {name: 'Yoko Shido', salary: 1500, increase: false, id: 5},
-        {name: 'Mala Karyka', salary: 350, increase: false, id: 6},
-    ];
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(elem => elem.id !== id)
+            }
+        })
+    }
 
-    return (
-        <div className='app' >
-            {/* <WhoAmI name='John' surname='Smith' link='google.com'/> */}
-            <AppInfo/>
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    render() {
+        return (
+            <div className='app' >
+                {/* <WhoAmI name='John' surname='Smith' link='google.com'/> */}
+                <AppInfo/>
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployersList data={this.state.data}
+                onDelete={this.deleteItem}/>
+                <EmployeesAddForm/>
             </div>
-
-            <EmployersList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    )
+        )
+    }
 }
 
 export default App;
