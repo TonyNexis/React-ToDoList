d80875d96087929bb43edd786a739a3897b003c9
Anton <tony.zenekis@gmail.com>
2023-07-01 16:09:04 +0300

    added error message when adding new employer

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -7,28 +7,43 @@ class EmployersAddForm extends Component {
         super(props);
         this.state = {
             name: '',
-            salary: ''
+            salary: '',
+            isErrorName: false,
+            isErrorSalary: false
         }
     }
 
     onValueChange = (e) => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
+            isErrorName: false,
+            isErrorSalary: false
         })
     }
 
     addEmployer = (e) => {
         e.preventDefault();
-
-        this.props.onAdd(this.state.name, this.state.salary)
-        this.setState({
-            name: '',
-            salary: ''
-        })
+        if (this.state.name.length < 3) {
+            this.setState({ isErrorName:true })
+            setTimeout(() => {
+                this.setState({ isErrorName:false })
+            }, 1000);
+        } else if (!this.state.salary) {
+            this.setState({ isErrorSalary:true })
+            setTimeout(() => {
+                this.setState({ isErrorSalary:false })
+            }, 1000);
+        } else {
+            this.props.onAdd(this.state.name, this.state.salary)
+            this.setState({
+                name: '',
+                salary: '',
+            })
+        }
     }
 
     render() {
-        const {name, salary} = this.state;
+        const {name, salary, isErrorName, isErrorSalary} = this.state;
 
         return (
             <div className="app-add-form">
@@ -36,13 +51,13 @@ class EmployersAddForm extends Component {
                 <form
                     className="add-form d-flex">
                     <input type="text"
-                        className="form-control new-post-label"
+                        className={`form-control new-post-label ${isErrorName ? 'highlight-animation' : ''}`}
                         placeholder="Как его зовут?"
                         name='name'
                         value={name}
                         onChange={this.onValueChange}/>
                     <input type="number"
-                        className="form-control new-post-label"
+                        className={`form-control new-post-label ${isErrorSalary ? 'highlight-animation' : ''}`}
                         placeholder="З/П в $?"
                         name='salary'
                         value={salary}
