036d11c4f44b3f02d62657ddfc0f4768c40b8591
Anton <tony.zenekis@gmail.com>
2023-07-03 22:05:51 +0300

    added filters for employers

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -21,7 +21,8 @@ class App extends Component {
                 {name: 'Yoko Shido', salary: 1500, increase: false, rise: false, id: 5},
                 {name: 'Mala Karyka', salary: 350, increase: false, rise: false, id: 6},
             ],
-            term: ''
+            term: '',
+            filter: 'all',
         }
     }
 
@@ -81,9 +82,26 @@ class App extends Component {
         this.setState({term})
     }
 
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+                case 'increase':
+                    return items.filter(item => item.increase);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render() {
-        const {data, term} = this.state;
-        const visibleData = this.searchEmp(data, term)
+        const {data, term, filter} = this.state;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
         return (
             <div className='app' >
@@ -91,7 +109,7 @@ class App extends Component {
                 <AppInfo data={this.state.data}/>
                 <div className="search-panel">
                     <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
-                    <AppFilter/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
     
                 <EmployersList 
